ce4297823298ced09b8a620d25367c76d009ab9c
Christian Schmitz <cschmitz398@gmail.com>
2024-08-26 15:18:00 -0600

    ALONZO_GENESIS_PARAMS instance copied from ledger-babbage

--- src/params/AlonzoGenesisParams.js
+++ src/params/AlonzoGenesisParams.js
@@ -1,3 +1,5 @@
+import { ALONZO_GENESIS_COST_MODEL_PARAMS } from "@helios-lang/uplc"
+
 /**
  * @typedef {import("@helios-lang/uplc").CostModelParamsV1} CostModelParamsV1
  */
@@ -31,3 +33,34 @@
  *   maxValueSize: number
  * }} AlonzoGenesisParams
  */
+
+/**
+ * @type {AlonzoGenesisParams}
+ */
+export const ALONZO_GENESIS_PARAMS = {
+    lovelacePerUTxOWord: 34482,
+    executionPrices: {
+        prSteps: {
+            numerator: 721,
+            denominator: 10000000
+        },
+        prMem: {
+            numerator: 577,
+            denominator: 10000
+        }
+    },
+    maxTxExUnits: {
+        exUnitsMem: 10000000,
+        exUnitsSteps: 10000000000
+    },
+    maxBlockExUnits: {
+        exUnitsMem: 50000000,
+        exUnitsSteps: 40000000000
+    },
+    maxValueSize: 5000,
+    collateralPercentage: 150,
+    maxCollateralInputs: 3,
+    costModels: {
+        PlutusV1: ALONZO_GENESIS_COST_MODEL_PARAMS
+    }
+}
--- src/params/index.js
+++ src/params/index.js
@@ -1,3 +1,5 @@
+export { ALONZO_GENESIS_PARAMS } from "./AlonzoGenesisParams.js"
+
 /**
  * @typedef {import("./AlonzoGenesisParams.js").AlonzoGenesisParams} AlonzoGenesisParams
  */
