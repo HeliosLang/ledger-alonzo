e318e7266e494c81599983368f73e7a654e89233
Christian Schmitz <cschmitz398@gmail.com>
2024-08-27 03:33:23 -0600

    exported CommonAlonzoBabbageParams

--- src/params/index.js
+++ src/params/index.js
@@ -2,6 +2,7 @@ export { ERA_GENESIS_PARAMS } from "./EraGenesisParams.js"
 export { COST_MODEL_PARAMS } from "./CostModelParams.js"
 
 /**
+ * @typedef {import("./EraGenesisParams.js").CommonAlonzoBabbageParams} CommonAlonzoBabbageParams
  * @typedef {import("./EraGenesisParams.js").EraGenesisParams} EraGenesisParams
  * @typedef {import("./CostModelParams.js").CostModelParams} CostModelParams
  */
